8a9a6179163df9d8bb735863492e1f061647c601
mayukorin <19t7b2fx@gmail.com>
2020-11-18 11:58:02 +0900

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 18 11:49:02 2020
+
+@author: Mayuko
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.post_list,name='post_list'),
+]
+
